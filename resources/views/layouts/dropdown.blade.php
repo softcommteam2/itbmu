8e9e610d50a38dc215b209cc61cf37c3b2f38098
WutYeePhyo1996 <wutyeephyo0807@gmail.com>
2021-05-19 12:33:07 +0630

    update

--- resources/views/layouts/dropdown.blade.php
+++ resources/views/layouts/dropdown.blade.php
@@ -30,6 +30,13 @@
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
           </svg>
         </button>
+        <a href="{{ route('logout') }}"
+        class="no-underline hover:underline"
+        onclick="event.preventDefault();
+            document.getElementById('logout-form').submit();">{{ __('Logout') }}</a>
+        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="hidden">
+            {{ csrf_field() }}
+        </form>
                     <!-- This example requires Tailwind CSS v2.0+ -->
         <div class="relative inline-block text-left">
             <div>
--- routes/web.php
+++ routes/web.php
@@ -2,22 +2,23 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
-use App\Http\Controllers\{CourseController, CourseDetailController};
+use App\Http\Controllers\{CourseController, CourseDetailController,HomeController,NewsController};
 
 Route::get('/', function () {
     return view('welcome');
 });
 Auth::routes();
 
-Route::get('/home', [\App\Http\Controllers\HomeController::class, 'index'])->name('home')->middleware('auth');
-
-Route::get('/test', function () {
-    return view('dropdown');
-});
-Route::resource('/news', \App\Http\Controllers\NewsController::class);
-
+// Route::get('/home', [HomeController::class, 'index']);
+Route::resource('/news', NewsController::class);
 Route::resource('courses', CourseController::class);
 Route::resource('course-details', CourseDetailController::class);
 
-
+Route::group(['prefix' => 'admin','middleware' => 'auth'], function() {
+    Route::get('/', [HomeController::class, 'index']);
+    Route::get('/home', [HomeController::class, 'index']);
+    Route::resource('/news', NewsController::class);
+    Route::resource('courses', CourseController::class);
+    Route::resource('course-details', CourseDetailController::class);
+});
 
