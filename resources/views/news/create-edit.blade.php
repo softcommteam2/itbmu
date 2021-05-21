b46981602ea6b097f22cfa644a99db65245af82c
WutYeePhyo1996 <wutyeephyo0807@gmail.com>
2021-05-21 09:30:33 +0630

    update

--- resources/views/news/create-edit.blade.php
+++ resources/views/news/create-edit.blade.php
@@ -99,7 +99,7 @@
                 <button type="button" class="px-4 py-2 text-sm font-medium text-gray-700 bg-yellow-400 border border-yellow-100 rounded-md shadow-sm hover:bg-yellow-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-300">
                 Cancel
                 </button>
-                <button type="submit" class="inline-flex justify-center px-4 py-2 ml-3 text-sm font-medium text-white bg-green-600 border border-transparent rounded-md shadow-sm hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-300">
+                <button type="submit" class="inline-flex justify-center px-4 py-2 ml-3 text-sm font-medium text-white bg-blue-600 border border-transparent rounded-md shadow-sm hover:bg-blue-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-300">
                 Save
                 </button>
             </div>
--- resources/views/news/index.blade.php
+++ resources/views/news/index.blade.php
@@ -7,9 +7,9 @@
     <div class="flex justify-between">
         <div><input type="text" placeholder="search"></div>
 
-        <button type="button" class="inline-flex items-center px-4 py-2 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+        <a href="{{url('/news/create')}}" class="inline-flex items-center px-4 py-2 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm button hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
             Add News
-          </button>
+        </a>
 
     </div>
     <div class="flex flex-col">
